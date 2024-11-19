c537bbab5c7a008b02351a74f4c4ec00f3399ba3
Andrew Ajamian <andrewaajamian@gmail.com>
2024-11-19 10:11:16 -0700

    update rx

--- Lab15_RX/main.py
+++ Lab15_RX/main.py
@@ -3,9 +3,8 @@ from machine import Pin, PWM, Timer
 from ir_rx.nec import NEC_8 # Use the NEC 8-bit class
 from ir_rx.print_error import print_error # for debugging
 
-global current_direction
-global previous_direction
-previous_direction = current_direction = -1
+global current_direction 
+current_direction = -1
 
 pwm_rate = 2000
 pwm = min(max(int(2**16 * abs(1)), 0), 65535)
@@ -21,43 +20,46 @@ def ir_callback(data, addr, _):
     current_direction = data
     motorDirection(data)
 
-def motorDirection(data):
-    while data == 0x01:
-        print("Motor FORWARDS") # Print to REPL
-        m1_ain1_ph.low()
-        m1_ain2_en.duty_u16(pwm)
-        m2_ain1_ph.low()
-        m2_ain2_en.duty_u16(pwm)
-    while data == 0x02:
-        print("Motor BACKWARDS") # Print to REPL
-        m1_ain1_ph.high()
-        m1_ain2_en.duty_u16(pwm)
-        m2_ain1_ph.high()
-        m2_ain2_en.duty_u16(pwm)
-    while data == 0x03: 
-        print("Motor LEFT") # Print to REPL
-        m1_ain1_ph.low()
-        m1_ain2_en.duty_u16(pwm)
-        m2_ain1_ph.high()
-        m2_ain2_en.duty_u16(pwm)
-    while data == 0x04: 
-        print("Motor RIGHT") # Print to REPL
-        m1_ain1_ph.high()
-        m1_ain2_en.duty_u16(pwm)
-        m2_ain1_ph.low()
-        m2_ain2_en.duty_u16(pwm) 
+def stopMotors():
     print("Motor OFF") # Print to REPL
     m1_ain1_ph.low()
     m1_ain2_en.duty_u16(0) 
     m2_ain1_ph.low()
     m2_ain2_en.duty_u16(0) 
 
+
+def motorDirection(data):
+    if data == 0x01:
+        print("Motor FORWARDS") # Print to REPL
+        m1_ain1_ph.low()
+        m1_ain2_en.duty_u16(pwm)
+        m2_ain1_ph.low()
+        m2_ain2_en.duty_u16(pwm)
+    elif data == 0x02:
+        print("Motor BACKWARDS") # Print to REPL
+        m1_ain1_ph.high()
+        m1_ain2_en.duty_u16(pwm)
+        m2_ain1_ph.high()
+        m2_ain2_en.duty_u16(pwm)
+    elif data == 0x03: 
+        print("Motor LEFT") # Print to REPL
+        m1_ain1_ph.low()
+        m1_ain2_en.duty_u16(pwm)
+        m2_ain1_ph.high()
+        m2_ain2_en.duty_u16(pwm)
+    elif data == 0x04: 
+        print("Motor RIGHT") # Print to REPL
+        m1_ain1_ph.high()
+        m1_ain2_en.duty_u16(pwm)
+        m2_ain1_ph.low()
+        m2_ain2_en.duty_u16(pwm) 
+    else:
+        stopMotors()
+
 def timer_callback(timer):
-    global previous_direction
-    if (current_direction != previous_direction):
-        print("Update Direction")
-        previous_direction = current_direction
-        motorDirection(current_direction)
+    global current_direction
+    if current_direction == 0x05:
+        stopMotors()
 
 # Setup the IR receiver
 ir_pin = Pin(17, Pin.IN, Pin.PULL_UP) # Adjust the pin number based on your wiring
